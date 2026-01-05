6ca4af3beb633853a4a4077e546c624219d1f1d6
Felipe C. Araujo <felipe.s-04@hotmail.com>
2026-01-05 18:29:47 -0300

    fix: ios sdk build

--- ios/BearoundReactSdkEventEmitter.h
+++
@@ -1,6 +0,0 @@
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface BearoundReactSdkEventEmitter : RCTEventEmitter <RCTBridgeModule>
-+ (void)emit:(NSString *)name body:(id)body;
-@end
